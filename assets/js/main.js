7defc3f622b6b83e8d564c98fbf032c7b873ea44
oMoFoLaYaN <nathanadeosun2003@gmail.com>
2023-01-19 18:42:06 +0100

    modified:   assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,17 +247,17 @@
 })()
 
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName('skills--content'),
-      skillsHeader = document.querySelectorAll('.skills--header')
+const skillsContent = document.getElementsByClassName('skills-content'),
+      skillsHeader = document.querySelectorAll('.skills-header')
 
 function toggleSkills(){
     let itemClass = this.parentNode.className
     
     for(i = 0; i < skillsContent.length; i++){
-        skillsContent[i].className = 'skills--content skills--close'
+        skillsContent[i].className = 'skills-content skills-close'
     }
-    if(itemClass === 'skills--content skills--close'){
-        this.parentNode.className = 'skills--content skills--open'
+    if(itemClass === 'skills-content skills-close'){
+        this.parentNode.className = 'skills-content skills-open'
     }
 }
 
